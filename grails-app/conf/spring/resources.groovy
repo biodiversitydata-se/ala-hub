56be8063c22999242ab1dc2a5cb997922f95c264
Mats Bovin <mats.bovin@gmail.com>
2023-11-10 11:32:29 +0100

    Fix issue with application not starting when security.oidc being disabled

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
+import org.pac4j.core.config.Config
+
 // Place your Spring DSL code here
 beans = {
+    // SBDI: Application won't start without this. It is related to security.oidc.enabled
+    // being set to false since we don't use it.
+    myConfig(Config)
 }
